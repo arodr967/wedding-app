2169e6d8ad6c3bd5a47abd9acca5567508d3f1e9
Alicia Rodriguez <rodriguez.aliciaf@gmail.com>
2018-07-07 19:04:00 -0400

    prepare for deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HomeService } from './home/services/home.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   template:  `
-    <h1>
-      {{title}}
-    </h1>
+    <h1>Wedding App</h1>
+    <ul>
+      <h3>Guests:</h3>
+      <li *ngFor="let guest of (guests | async)">{{ guest.firstName }} {{ guest.lastName }}</li>
+    </ul>
   `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'app works!';
+export class AppComponent {
+  guests: Observable<any>;
 
-  constructor(private homeService: HomeService) { }
-
-  ngOnInit() {
-    this.homeService.getGuests()
-      .subscribe(data => {
-        console.log('data', data);
-      });
+  constructor(private homeService: HomeService) {
+    this.guests = this.homeService.getGuests();
   }
 }
